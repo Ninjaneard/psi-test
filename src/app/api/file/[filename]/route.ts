7dd85a4b0e3e87893c54190bab7dbe100b37226d
neca <neca.87@gmail.com>
2026-02-19 20:18:21 +0100

    Test reziltati fix

--- src/app/api/file/[filename]/route.ts
+++ src/app/api/file/[filename]/route.ts
@@ -1,9 +1,14 @@
 import { readFile } from 'node:fs/promises';
 import path from 'node:path';
+import { NextRequest } from 'next/server';
+type Props = {
+    params: Promise<{ filename: string }>; // Change to Promise
+};
 
-export async function GET(req: Request, { params }: { params: { filename: string } }) {
+export async function GET(req: NextRequest, { params }: { params: Props }) {
     // Define your storage path (outside of /public)
     console.log(params);
+    // @ts-ignore
     const {filename} = await params;
     console.log(filename);
     const filePath = path.join(process.cwd(), 'public', filename);
